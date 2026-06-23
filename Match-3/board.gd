7f13e31381809220b0322dcc796c8af6db4c4eaa
Alexandr <112582926+NineteenthPilgrim@users.noreply.github.com>
2026-06-23 18:45:19 +0200

    Added ability to select elements on the board

--- Match-3/board.gd
+++ Match-3/board.gd
@@ -4,6 +4,7 @@ const Rows = 8
 const Cols = 8
 const Tile_Size = 32
 var Grid = []
+var Selected_Piece = null #create variable to store selected element
 
 func _ready():
 	print("Сетка готова: ", Rows, "x", Cols)
@@ -19,6 +20,7 @@ func _ready():
 		for col in range(Cols):
 			var piece = Area2D.new()	#create element
 			var sprite = Sprite2D.new()	#create texture
+			sprite.name = "Sprite2D"
 			
 			piece.position = Vector2(col * Tile_Size + Tile_Size/2, row * Tile_Size + Tile_Size/2)	#position of element
 			sprite.texture = textures[randi() % textures.size()]	#select texture
@@ -29,14 +31,21 @@ func _ready():
 			collision.shape.extents = Vector2(Tile_Size/2, Tile_Size/2)
 			piece.add_child(collision)
 			
-			piece.connect("input_event", Callable(self, "_on_piece_clicked")) #connect signal
+			piece.connect("input_event", Callable(self, "_on_piece_clicked").bind(piece)) #connect signal
 			add_child(piece)
 			Grid[row].append(piece)		#save element to array
 
-func _on_piece_clicked(viewport, event, shape_index):
+func _on_piece_clicked(viewport, event, shape_index, piece):	#call function on element click
 	if event is InputEventMouseButton and event.pressed:
-		print("Element selected")
-
+		if Selected_Piece == null:
+			var sprite = piece.get_node("Sprite2D")					#first element selected
+			if sprite != null:
+				Selected_Piece = sprite
+				Selected_Piece.modulate = Color(0.2, 0.2, 0.2)		#highlight element
+				print("First element selected")
+		else:
+			Selected_Piece.modulate = Color(1, 1, 1)
+			Selected_Piece = null
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
