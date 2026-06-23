132213f22996c26eaaff8bb4a136475e26b1a24d
Alexandr <112582926+NineteenthPilgrim@users.noreply.github.com>
2026-06-23 11:52:27 +0200

    Add files via upload
    - added element generation and click handling

--- Match-3/board.gd
+++ Match-3/board.gd
@@ -17,11 +17,25 @@ func _ready():
 	for row in range(Rows):
 		Grid[row]= []
 		for col in range(Cols):
-			var sprite = Sprite2D.new()
-			sprite.texture = textures[randi() % textures.size()]
-			sprite.position = Vector2(col * Tile_Size + Tile_Size/2, row * Tile_Size + Tile_Size/2)
-			add_child(sprite)
-			Grid[row].append(sprite)
+			var piece = Area2D.new()	#create element
+			var sprite = Sprite2D.new()	#create texture
+			
+			piece.position = Vector2(col * Tile_Size + Tile_Size/2, row * Tile_Size + Tile_Size/2)	#position of element
+			sprite.texture = textures[randi() % textures.size()]	#select texture
+			piece.add_child(sprite)		#add image to node
+			
+			var collision = CollisionShape2D.new()		#create click area
+			collision.shape = RectangleShape2D.new()	#create ectangle shape
+			collision.shape.extents = Vector2(Tile_Size/2, Tile_Size/2)
+			piece.add_child(collision)
+			
+			piece.connect("input_event", Callable(self, "_on_piece_clicked")) #connect signal
+			add_child(piece)
+			Grid[row].append(piece)		#save element to array
+
+func _on_piece_clicked(viewport, event, shape_index):
+	if event is InputEventMouseButton and event.pressed:
+		print("Element selected")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
