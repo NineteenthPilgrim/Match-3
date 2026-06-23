c84bafe91e711a4495e30ce378662c6c305cd330
Alexandr <112582926+NineteenthPilgrim@users.noreply.github.com>
2026-06-23 21:06:21 +0200

    Added ability to swap adjacent elements

--- Match-3/board.gd
+++ Match-3/board.gd
@@ -5,6 +5,8 @@ const Cols = 8
 const Tile_Size = 32
 var Grid = []
 var Selected_Piece = null #create variable to store selected element
+var Selected_Sprite = null 
+
 
 func _ready():
 	print("Сетка готова: ", Rows, "x", Cols)
@@ -35,17 +37,37 @@ func _ready():
 			add_child(piece)
 			Grid[row].append(piece)		#save element to array
 
+
 func _on_piece_clicked(viewport, event, shape_index, piece):	#call function on element click
 	if event is InputEventMouseButton and event.pressed:
 		if Selected_Piece == null:
 			var sprite = piece.get_node("Sprite2D")					#first element selected
 			if sprite != null:
-				Selected_Piece = sprite
+				Selected_Piece = piece
+				Selected_Sprite = sprite
 				Selected_Piece.modulate = Color(0.2, 0.2, 0.2)		#highlight element
 				print("First element selected")
 		else:
+			if neighbors(Selected_Piece.position, piece.position):
+				swap_pieces(Selected_Piece, piece)
+				print("Pieces swapped")
+			else:
+				print("Not neighbors")
 			Selected_Piece.modulate = Color(1, 1, 1)
 			Selected_Piece = null
+			Selected_Sprite = null
+			print("First element is no longer selected")
+
+
+func neighbors(Pos1: Vector2, Pos2: Vector2) -> bool:
+	var Diff = Pos1 - Pos2
+	return (abs(Diff.x) == Tile_Size and Diff.y == 0) or (abs(Diff.y) == Tile_Size and Diff.x == 0)
+
+
+func swap_pieces(Piece1: Area2D, Piece2: Area2D):
+	var Temp_Pos = Piece1.position
+	Piece1.position = Piece2.position
+	Piece2.position = Temp_Pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
